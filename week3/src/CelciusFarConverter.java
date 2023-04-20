c41250dfdeb03888a9381483ef415bec6f66c4f5
tuktabe <129831697+tuktabe@users.noreply.github.com>
2023-04-20 13:54:57 -0700

    Add files via upload

---
+++ week3/src/CelciusFarConverter.java
@@ -0,0 +1,17 @@
+public class CelciusFarConverter {
+
+
+    public static void main(String[] args) {
+        // far = (cel * 9/5) + 32
+
+        int cel, far;
+
+        for(cel = 0; cel <= 100; cel = cel+1) {
+            far = (cel * 9/5) + 32;
+            System.out.println(cel + " degrees Celcius is equal to " +far+ "degrees Farenheit");
+
+
+        } // end for loop
+        System.out.println("All done!");
+    }
+}
